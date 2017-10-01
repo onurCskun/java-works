de84f38ca3d2263b67329fab6dc5bba787aa521b
Onur COSKUN <onurcskunimp@gmail.com>
2017-10-01 18:26:24 +0300

    Add files via upload

---
+++ algorithms/search/BinarySearch.java
@@ -0,0 +1,39 @@
+package algorithms;
+
+public class BinarySearch {
+
+	public static void main(String args[]) {
+		int list[] = { 17, 27, 65, 70, 81, 82, 88, 90, 92, 94 };
+		int num = 88;
+		
+		search(list, num);
+		
+	}
+
+	public static void search(int[] arr, int num) {
+		int arrL = arr.length;
+		int result = binarySearch(arr, 0, arrL - 1, num);
+
+		if (result == -1)
+			System.out.println("The list does not contain " + num);
+		else
+			System.out.println("The element found at index " + result);
+		
+	}
+
+	public static int binarySearch(int arr[], int lowP, int highP, int num) {
+		if (highP >= lowP) {
+			int midP = lowP + (highP - lowP) / 2;
+
+			if (arr[midP] == num)
+				return midP;
+			if (arr[midP] > num)
+				return binarySearch(arr, lowP, midP - 1, num);
+			else
+				return binarySearch(arr, midP + 1, highP, num);
+			
+		}
+		return -1;
+		
+	}
+}
